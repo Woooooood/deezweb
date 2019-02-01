effe1ada04887281bb7f1c3b4b958a67a13217a0
Woooooood <wassim.boulatika@supinternet.fr>
2019-02-01 18:05:23 +0100

    added favorite vue and function addFav but there is nothing inside for the moment, removed broken css

--- src/components/Favorite.vue
+++ src/components/Favorite.vue
@@ -1,11 +1,31 @@
 <template>
-<div></div>
+<div>
+    <p>Fonctionnalité à venir !</p>
+    <h1>Mes favoris </h1> <br>
+    <p>Listes de mes favoris</p>
+    <div v-if="favsong">
+        <MusicCard v-for="(item, index) in song.data" :key="index" :song="item" />
+    </div>
+    <div v-else>
+        Aucun favoris dans votre liste ...
+    </div>
+
+</div>
     
 </template>
 
 <script>
+import MusicCard from '@/components/MusicCard.vue';
 export default {
-
+    name : 'Favorite',
+    components : {
+        MusicCard,
+    },
+    data() {
+        return {
+            favsong : null,      
+        };
+    },
 }
 </script>
 
--- src/components/MusicCard.vue
+++ src/components/MusicCard.vue
@@ -5,12 +5,14 @@
                 <figure>
                 <img :src="song.artist.picture" alt="">
                 </figure>
-                <h1>Titre : {{song.title}}</h1>
-                <p>Artiste : {{song.artist.name}}</p>
+                <h2>Titre : {{song.title}}</h2>
+                <h3>Artiste : {{song.artist.name}}</h3>
             </div>
             <audio controls :src="song.preview" type="audio/mpeg"/>
             <br>
-            <a href="#">add to favorite</a>
+            <a href="#" @click.prevent="addFav">
+                <p>Add to Favorite</p>
+                </a>
             <br>
         </div>
     </div>
@@ -24,50 +26,23 @@ export default {
     },
     data() {
         return {
-            
+            faved : '',
         }
     },
     methods: {
-        
+       addFav(){
+           
+       },
+
+       getFav(){
+
+       },
+
     },
 }
 </script>
 <style scoped lang="scss">
 
-.grid{
-     display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-gap: 10px;
-}
-.music-card {
-    display: flex;
-    flex-direction: row;
-    align-items: stretch;
-    box-shadow: 0 0 15px #c0c0c0;
-    border-radius: 2px;
-    background-color: #fafafa;
-    overflow: auto;
-    text-align: left;
-    padding: 0.25em 1.25em 0.25em 0;
-}
 
-.music-card > figure {
-    margin: 0.25em;
-    text-align: center;
-    display: flex;
-    align-items: center;
-}
-
-.music-card > figure > img {
-    display: block;
-    border-radius: 300px;
-    max-width: 128px;
-}
-
-.music-card > div {
-    margin: 0.25em;
-    font-size: 0.8em;
-    line-height: 1.6em;
-}
 </style>
 
